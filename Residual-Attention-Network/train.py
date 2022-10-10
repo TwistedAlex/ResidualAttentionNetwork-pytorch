c2e35171277078e665ee4c0426f9b58aec8a1199
TwistedAlex <muzihuole@gmail.com>
2022-10-10 16:16:14 -0500

    add extra args

--- Residual-Attention-Network/train.py
+++ Residual-Attention-Network/train.py
@@ -80,7 +80,8 @@ parser.add_argument('--input_dir', help='path to the input idr', type=str)
 parser.add_argument('--batch_pos_dist', type=float, help='positive relative amount in a batch', default=0.5)
 parser.add_argument('--nepoch', type=int, default=6000, help='number of iterations per epoch')
 parser.add_argument('--deviceID', type=int, help='deviceID', default=0)
-
+parser.add_argument('--masks_to_use', type=float, default=0.1,
+                    help='the relative number of masks to use in ex-supevision training')
 
 # Image Preprocessing
 def main(args):
